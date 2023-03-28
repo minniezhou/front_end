d184bb2082e8d36ba7180ff162049a0c173bc946
Min Zhou <min.zhou@salesforce.com>
2023-03-28 09:27:18 -0400

    component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Buttons from "./components/Buttons";
+import Text from "./components/Text";
+import TextBox from "./components/TextBox";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Buttons></Buttons>
+      <TextBox text="Output shows here..."></TextBox>
+      <div>
+        <TextBox text="Sent..."></TextBox>
+        <TextBox text="Received..."></TextBox>
+      </div>
+      <Footer />
     </div>
   );
 }
