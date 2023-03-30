5e5fd47314bfff42af6362e96a94db9f6e4da2ba
min.zhou <min.zhou@minzhou-ltmxp7f.internal.salesforce.com>
2023-03-30 10:10:10 -0400

    buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,40 +1,7 @@
-import { useContext } from "react";
-import { InandOutCxt } from "../store/context";
 import "./Button.css";
-const Button: React.FC<{ name: string }> = (prop) => {
-  const cxt = useContext(InandOutCxt);
+const Button: React.FC<{ name: string; OnClick: () => void }> = (prop) => {
   return (
-    <button
-      onClick={() => {
-        fetch("http://localhost:8080", {
-          method: "POST",
-        })
-          .then((response) => {
-            if (!response.ok) {
-              cxt.SetError("fetching data error...");
-              return;
-            }
-            return response.json();
-          })
-          .then((data) => {
-            if (data.error) {
-              cxt.SetError(data.message);
-              return;
-            }
-            const output = JSON.stringify(data);
-            cxt.SetRequest("empty request...");
-            cxt.SetResponse(output);
-          })
-          .catch((error) => {
-            if (error) {
-              cxt.SetError(error.message);
-            } else {
-              cxt.SetError("Connection error");
-            }
-          });
-      }}
-      className="button"
-    >
+    <button onClick={prop.OnClick} className="button">
       {prop.name}
     </button>
   );
--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,10 +1,71 @@
 import Button from "./Button";
-
+import { InandOutCxt } from "../store/context";
+import { useContext } from "react";
 const Buttons = () => {
+  const cxt = useContext(InandOutCxt);
+  type ClickInput = {
+    url: string;
+    method: string;
+    input: string;
+  };
+  const OnClick = (In: ClickInput) => {
+    fetch(In.url, {
+      method: In.method,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          cxt.SetError("fetching data error...");
+          return;
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.error) {
+          cxt.SetError(data.message);
+          return;
+        }
+        const output = JSON.stringify(data);
+        cxt.ClearInandOut();
+        cxt.SetRequest(In.input);
+        cxt.SetResponse(output);
+      })
+      .catch((error) => {
+        cxt.ClearInandOut();
+        if (error) {
+          cxt.SetError(error.message);
+        } else {
+          cxt.SetError("Connection error");
+        }
+      });
+  };
   return (
     <>
-      <Button name="Test Broker"></Button>
-      <Button name="Test Auth"></Button>
+      <Button
+        name="Test Broker"
+        OnClick={() => {
+          OnClick({
+            url: "http://localhost:8080",
+            method: "POST",
+            input: "empty request",
+          });
+        }}
+      ></Button>
+      <Button
+        OnClick={() => {
+          OnClick({
+            url: "http://localhost:8080/hello",
+            method: "GET",
+            input: "empty request",
+          });
+        }}
+        name="Test Auth"
+      ></Button>
+      <Button
+        OnClick={() => {
+          cxt.ClearInandOut();
+        }}
+        name="Clear"
+      ></Button>
     </>
   );
 };
